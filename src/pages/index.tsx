16942122c07cff2fc3f2742a679d80ed5bc40eae
HizkiaPalar <Hizkiapalar294@gmail.com>
2024-03-30 13:49:04 +0800

    feat: slicing CashOnBank page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,5 +2,7 @@ import SplashScreen from './SplashScreen';
 import SignIn from './SignIn';
 import SignUp from './SignUp';
 import Home from './Home';
+import CashonHand from './CashonHand';
+import CashonBank from './CashonBank';
 
-export {SplashScreen, SignIn, SignUp, Home};
+export {SplashScreen, SignIn, SignUp, Home, CashonHand, CashonBank};
